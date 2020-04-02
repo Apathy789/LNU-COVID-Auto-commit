6dd9207a430bb53daab1c0b874010cf793641148
JessyTsu1 <51992423+JessyTsu1@users.noreply.github.com>
2020-04-02 09:50:45 +0800

    增加批量打卡
    将主题写成了接口，主函数中循环调用实现批量打卡

--- 辽大打卡.py
+++ 辽大打卡.py
@@ -1,26 +1,21 @@
 import os
 import time
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 
-if __name__ == '__main__':
-    #这里改成你的统一认证用户名和密码
-    user_name = 'xxxx'
-    pwd = 'xxxx'
 
-    # 加上这两句话不打开浏览器
+def run():
     option = webdriver.ChromeOptions()
-    # option.add_argument('headless') # 设置option
     # 用浏览器打开打卡的网址
     browser = webdriver.Chrome(options=option)
     browser.get("http://tjxx.lnu.edu.cn/")
 
     # 输用户名和密码
-
-    user_name_input = browser.find_element_by_name("userid")
-    user_name_input.send_keys(user_name)
+    stuID_input = browser.find_element_by_name("userid")
+    stuID_input.send_keys(stuID)
 
     user_pwd_input = browser.find_element_by_name("userpwd")
     user_pwd_input.send_keys(pwd)
@@ -30,31 +25,44 @@ if __name__ == '__main__':
     ActionChains(browser).move_to_element(login_button).click(login_button).perform()
     print('登陆')
 
-    # browser.get("http://tjxx.lnu.edu.cn/")
-
     # 上次填报以来有无城市间流动经历？
-
-    browser.find_element_by_xpath( '//*[@id="sub_form"]/div[3]/div[2]/label[1]').click()
+    browser.find_element_by_xpath('//*[@id="sub_form"]/div[3]/div[2]/label[1]').click()
 
     # 当日有无湖北（武汉）进出人员接触史？
     browser.find_element_by_xpath('//*[@id="sub_form"]/div[4]/div[2]/label[1]').click()
 
     # 当日身体症状?
-    browser.find_element_by_xpath( '//*[@id="sub_form"]/div[5]/div[2]/label[1]').click()
+    browser.find_element_by_xpath('//*[@id="sub_form"]/div[5]/div[2]/label[1]').click()
 
     # 当前是否隔离观察？
-    browser.find_element_by_xpath( '//*[@id="sub_form"]/div[6]/div[2]/label[1]').click()
+    browser.find_element_by_xpath('//*[@id="sub_form"]/div[6]/div[2]/label[1]').click()
 
     # 是否在校在岗？
-    browser.find_element_by_xpath( '//*[@id="sub_form"]/div[7]/div[2]/label[1]').click()
+    browser.find_element_by_xpath('//*[@id="sub_form"]/div[7]/div[2]/label[1]').click()
 
     # 提交
     browser.find_element_by_xpath('//*[@id="formSubmitBtn"]').click()
 
-    time.sleep(5)
+    time.sleep(3)
     # 确认提交
     browser.find_element_by_xpath('/html/body/div[3]/div[2]/div[3]/a[2]').click()
 
-    time.sleep(5)
-    # #关闭浏览器
+    time.sleep(3)
+    # 关闭浏览器
     browser.quit()
+
+
+if __name__ == '__main__':
+    # 填写学号
+    stuIDs = ['xxxx', 'xxxx']
+    # 填写平台密码
+    pwds = ['xxxx', 'xxxx']
+
+    for i in range(0, len(stuIDs)):
+        stuID = stuIDs[i]
+        pwd = pwds[i]
+        run()
+        print(stuIDs[i] + '运行成功')
+        time.sleep(3)
+        
+    print('Everything is OK')
