132e8c574b1c77e57b91388cc547c150c175cb7f
Jessy <jiaxicui446@gmail.com>
2021-11-14 12:11:23 +0800

    更新验证码识别及部分选项改动

--- auto-COVID-commit.py
+++ auto-COVID-commit.py
@@ -4,11 +4,12 @@
 Made by @Jessy 2020/03/28
 """
 
-
+import BaiDuAPI
 import sys
 import os
 import time
 import config
+from PIL import Image
 from config import stuIDs, pwds
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -47,8 +48,9 @@ def run():
     option = webdriver.ChromeOptions()
     # 用浏览器打开打卡的网址
     browser = webdriver.Chrome(options=option)
-    target='http://tjxx.lnu.edu.cn/inputExt.asp'
+    target='http://tjxx.lnu.edu.cn/login.asp'
     browser.get(target)
+    webdriver.Chrome(options=option)
 
     # 输用户名和密码
     stuID_input = browser.find_element_by_name("userid")
@@ -57,17 +59,41 @@ def run():
     user_pwd_input = browser.find_element_by_name("userpwd")
     user_pwd_input.send_keys(pwd)
 
+    browser.save_screenshot('./full.png')  # 截图
+    image_code = browser.find_element_by_id('checkimg')
+    ## mac需要都乘2
+    left = image_code.location['x'] * 2  # 区块截图左上角在网页中的x坐标
+    top = image_code.location['y'] * 2  # 区块截图左上角在网页中的y坐标
+    right = left + image_code.size['width'] * 2  # 区块截图右下角在网页中的x坐标
+    bottom = top + image_code.size['height'] * 2  # 区块截图右下角在网页中的y坐标
+    # print({"left": left, "top": top, "right": right, "bottom ": bottom})
+    # print("image_code.size['width']:%s" % image_code.size['width'])  ##width:80
+    # print("image_code.size['height']:%s" % image_code.size['height'])  ## hight:20
+    picture = Image.open('./full.png')
+    picture = picture.crop((left, top, right, bottom))  # 二次截图：形成区块截图
+    picture.save('./code.png')
+
+    BaiDuAPI.baiduCode.get_token()
+    code = BaiDuAPI.baiduCode.get_code(path='./code.png')
+
+    checkcode = browser.find_element_by_name("checkcode")
+    checkcode.send_keys(code)
+
     # 登陆
     login_button = browser.find_element_by_id("formSubmitBtn")
     ActionChains(browser).move_to_element(login_button).click(login_button).perform()
     printLog(stuIDs[i] + '登陆中')
-    #print('登陆中')
+    print('登陆中')
 
     if browser.current_url == target:
         printLog('登陆失败')
         browser.quit()
     else:
         printLog('登陆成功')
+
+    #体温
+    temperature = browser.find_element_by_name("drtw")
+    temperature.send_keys('36.5')
 
     # 上次填报以来有无城市间流动经历？
     browser.find_element_by_xpath('//label[@class="weui-cell weui-check__label" and @for="r1"]').click()
@@ -82,7 +108,10 @@ def run():
     browser.find_element_by_xpath('//label[@class="weui-cell weui-check__label" and @for="r10"]').click()
 
     # 是否在校在岗？
-    browser.find_element_by_xpath('//label[@class="weui-cell weui-check__label" and @for="r5"]').click()
+    browser.find_element_by_xpath('//label[@class="weui-cell weui-check__label" and @for="r7"]').click()
+
+    # 是否在校在岗？
+    browser.find_element_by_xpath('//label[@class="weui-cell weui-check__label" and @for="r91"]').click()
 
     # 提交
     browser.find_element_by_xpath('//*[@id="formSubmitBtn"]').click()
@@ -107,7 +136,7 @@ if __name__ == '__main__':
         stuID = stuIDs[i]
         pwd = pwds[i]
 
-        while (not ifSuccess) and tryTimes < 3:
+        while (not ifSuccess) and tryTimes < 5:
             try:
                 tryTimes += 1
                 printLog('Trying ' + str(tryTimes) + ' times...')
